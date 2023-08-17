c4e81491f3e5996ec55476b2dbfc26d4e96321e3
BluePrintYang <yanglonglong99@gmail.com>
2023-08-17 22:28:39 +0800

    ds & co frame

---
+++ CS/Data-Structure/src/chap1/chap1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // 1.2 T6
+    int n = 9;
+    int m = 0, i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j <= 2 * i; j++)
+        {
+            m++;
+        }
+    }
+    printf("m=%d\n", m);
+
+    // 1.2 T13 【2022统考】
+    for (int n = 0; n < 100; n++)
+    {
+        int sum = 0;
+        for (int i = 1; i < n; i *= 2)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                sum++;
+            }
+        }
+        printf("n=%d,sum=%d\n", n, sum);
+    }
+
+    return 0;
+}
